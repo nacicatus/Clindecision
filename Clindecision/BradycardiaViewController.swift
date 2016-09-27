b886b80c1c0c8e70c5f5813f03d458784bd138ca
SaurabhSikka <drsaurabhsikka@gmail.com>
2016-09-28 00:19:59 +0200

    physiologic causes of bradycardia

--- Clindecision/BradycardiaViewController.swift
+++ Clindecision/BradycardiaViewController.swift
@@ -8,40 +8,64 @@
 
 import UIKit
 
-class BradycardiaViewController: UIViewController, UIPopoverPresentationControllerDelegate {
-
-    let hAndPText = "Bradycardia can be physiologic, pathologic, or pharmacologic in origin. Alteration of either the sinoatrial (SA) or atrioventricular (AV) conduction system may play a role. Symptoms of bradyarrhythmias include fatigue, dizziness or syncope, and dyspnea and angina."
+class BradycardiaViewController: UIViewController {
+    
+    
+    @IBOutlet weak var introLabel: UILabel!
+    @IBOutlet weak var historyDetailLabel: UILabel!
+    @IBOutlet weak var physioButton: UIButton!
+    @IBOutlet weak var pathoButton: UIButton!
+    @IBOutlet weak var pharmaButton: UIButton!
+    @IBOutlet weak var physioDifferentialText: UITextView!
+    
+    @IBOutlet weak var physioPossibleLabel: UILabel!
+    
+    @IBOutlet weak var physioCauseSwitch: UISwitch!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        introLabel.text = "Symptoms of bradyarrhythmias include fatigue, dizziness or syncope, and dyspnea and angina. \nAlteration of either the sinoatrial (SA) or atrioventricular (AV) conduction system may play a role."
+        historyDetailLabel.hidden = true
+        physioButton.hidden = true
+        pathoButton.hidden = true
+        pharmaButton.hidden = true
+        
+        physioDifferentialText.hidden = true
+        physioPossibleLabel.hidden = true
+        physioCauseSwitch.hidden = true
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // History
     
-//    struct Popsicle {
-//        var hAndPSegueIdent = "Show History Physical"
-//    }
+    @IBAction func historyButton(sender: UIButton) {
+        historyDetailLabel.hidden = false
+        historyDetailLabel.text = "After confirming bradycardia on EKG, the following causes may be explored:"
+        
+        physioButton.hidden = false
+        pathoButton.hidden = false
+        pharmaButton.hidden = false
+        
+    }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let identifier = segue.identifier {
-            switch identifier {
-            case "Show History Physical":
-                if let tvc = segue.destinationViewController as? TextViewController {
-                    if let ppc = tvc.popoverPresentationController {
-                        ppc.delegate = self
-                    }
-                    tvc.text = "\(hAndPText)"
-                }
-            default: break
-            }
+    @IBAction func physioButtonPressed(sender: UIButton) {
+        
+        if physioDifferentialText.hidden {
+            physioCauseSwitch.hidden = false
+            physioPossibleLabel.hidden = false
+            physioDifferentialText.hidden = false
+        } else {
+            physioCauseSwitch.hidden = true
+            physioPossibleLabel.hidden = true
+            physioDifferentialText.hidden = true
         }
+        
     }
     
-    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
-        return UIModalPresentationStyle.None
-    }
-
 }
